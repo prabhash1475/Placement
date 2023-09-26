6bb1f8d3918a62f72fe95016901cf87d4cfa904e
Prabhash Kumar <prabhash.kumar96@gmail.com>
2023-09-26 19:47:47 +0530

    hoistion updated

--- javaScript/q3.js
+++ javaScript/q3.js
@@ -9,10 +9,20 @@ function greetings() {
   console.log("Hello");
 }
 
-console.log(myName); //ReferenceError: Cannot access 'myName' before initialization
-myName();
+// console.log(myName); //ReferenceError: Cannot access 'myName' before initialization
+// myName();
 let myName = () => {
   console.log("Prabhash");
 };
 
 // function expression and arrow functions are not hoisted
+
+//------hoistin in let and const-----//
+console.log(num1); //ReferenceError: num1 is not defined
+
+console.log(num); //ReferenceError: Cannot access 'num' before initialization
+
+let num = 7;
+/* in let and const hoisting work diffrentely 
+it will give us refrence error cant access befor 
+initialization  */
