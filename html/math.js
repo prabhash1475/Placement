5a2518aec5f955645720c7e3931532b6eaed7040
Prabhash Kumar <prabhash.kumar96@gmail.com>
2023-10-04 02:25:48 +0530

    folder name updated

---
+++ html/math.js
@@ -0,0 +1,10 @@
+let heading = document.querySelector(".heading");
+heading.innerText = "Maths";
+let body = document.querySelector("body");
+let text = document.getElementById("text"); 
+
+function targetInput(){
+    let text = document.getElementById("text");
+    
+}
+body.appendChild(heading);
