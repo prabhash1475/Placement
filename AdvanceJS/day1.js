b591a6774f9a60f0f21bc4f5063e26a3b0590523
Prabhash Kumar <prabhash.kumar96@gmail.com>
2024-02-19 00:14:34 +0530

    advance js

---
+++ AdvanceJS/day1.js
@@ -0,0 +1,22 @@
+console.log(typeof null);
+// object
+console.log(1 == "1");
+// --> true
+console.log(1 === "1");
+//  --> false
+
+// declaration || assignment || reassignment
+
+/* 
+declaration
+ --> var name
+
+assignment 
+--> var name= "Prabhash"
+
+reassignment 
+
+ --> name= "Pablo"
+
+
+*/
