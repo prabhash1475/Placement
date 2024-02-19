9069d0065d2e2d06af759d97c14367b46ffb1630
Prabhash Kumar <prabhash.kumar96@gmail.com>
2024-02-19 12:48:03 +0530

    varables exucation context

--- AdvanceJS/day1.js
+++ AdvanceJS/day1.js
@@ -1,8 +1,10 @@
-console.log(typeof null);
+// console.log(typeof null);
 // object
-console.log(1 == "1");
+
+// console.log(1 == "1");
 // --> true
-console.log(1 === "1");
+
+// console.log(1 === "1");
 //  --> false
 
 // declaration || assignment || reassignment
@@ -20,3 +22,48 @@ reassignment
 
 
 */
+
+// ------ Variable-------- //
+/* 
+0.It Showing Information 
+1. Datatype (primitive || non-primitive)
+
+var arr = [1,2,3]
+--> 1,2,3
+arr[0]=5
+--> 5,2,3
+
+var str= "raju"
+--> raju
+
+str[0]="k"
+-->raju 
+
+---> arr vs arr[0]
+= arr gives us whole arr
+
+arr[0] 
+= gives us element of arr
+
+** arrays are [Mutable] and Strings are immutable **
+
+---Bank 
+    cash-yes (by value)
+    gold-yes (by value)
+    real-state - (document)  -yes (by location / reference)
+
+
+**  primitive -> it directly point to value by --- by value
+**  non-primitive -> it point to address (location) -- by reference 
+
+variables are not values
+variables are point to information. Sometime direct to value or reference (address)
+
+
+*/
+// console.log(i);
+// function add() {
+//   i = 9;
+//   //   console.log(i);
+// }
+// add();
